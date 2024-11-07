c6a3908ff52782f6a31736dde995a1e2e4f07672
thaya242 <parismoore2905@gmail.com>
2024-11-07 11:27:49 -0700

    MOB.java

--- src/MOB.java
+++ src/MOB.java
@@ -1,30 +1,46 @@
 public class MOB implements Attributes {
+    private String name;
+    private int maxHP;
+    private int armor;
+    private int hitModifier;
+    private DiceType damageDie;
+
+    public MOB(String name, int maxHP, int armor, int hitModifier, DiceType damageDie) {
+        this.name = name;
+        this.maxHP = maxHP;
+        this.armor = armor;
+        this.hitModifier = hitModifier;
+        this.damageDie = damageDie;
+    }
 
     @Override
     public int getArmor() {
-        // TODO Auto-generated method stub
-        return 0;
+        return armor;
     }
 
     @Override
     public int getMaxHP() {
-        // TODO Auto-generated method stub
-        return 0;
+        return maxHP;
     }
 
     @Override
     public DiceType getDamageDie() {
-        // TODO Auto-generated method stub
-        return null;
+        return damageDie;
     }
 
     @Override
     public int getHitModifier() {
-        // TODO Auto-generated method stub
-        return 0;
+        return hitModifier;
     }
-    
+
+    @Override
+    public String toString() {
+        return String.format("MOB: %s, HP: %d, Armor: %d, Hit Modifier: %d, Damage Die: %s", 
+                              name, maxHP, armor, hitModifier, damageDie);
+    }
+
     public static void main(String[] args) {
-        
+        MOB mob = new MOB("Orc", 25, 5, 2, DiceType.D6);
+        System.out.println(mob);
     }
 }
